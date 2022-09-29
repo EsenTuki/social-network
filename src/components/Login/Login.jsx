5f132baa2395633a75609c22494df17cae2f8d84
Sagyndyk0305 <92786577+Sagyndyk0305@users.noreply.github.com>
2022-09-29 17:37:23 +0600

    update LoginForm with redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,38 @@
 import React from "react"
+import { Field, reduxForm } from "redux-form"
 
 const Login = (props) => {
-    return <h1>Login</h1>
+
+    const onSugmit=(formData)=>{
+        console.log(formData)
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSugmit}/>
+    </div>
 }
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder="Login" name={"login"} component={"input"} />
+            </div>
+            <div>
+                <Field placeholder="Password" name={"password"} component={"input"} />
+            </div>
+            <div>
+                <Field type="checkbox" name={"rememberMe"} component={"input"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
+
 
 export default Login
